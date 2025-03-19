837636a510340ef61515e727b3972744e7874323
Leonid390 <leonidmashkov390@gmail.com>
2025-03-19 20:33:07 +0300

    logic.py

--- logic.py
+++ logic.py
@@ -1,64 +1,54 @@
+import telebot 
+from logic import Wizard, Fighter
+from config import token
 from random import randint
-import requests
+from logic import Pokemon
+bot = telebot.TeleBot(token) 
 
-class Pokemon:
-    pokemons = {}
-    # Инициализация объекта (конструктор)
-    def __init__(self, pokemon_trainer):
-
-        self.pokemon_trainer = pokemon_trainer   
-
-        self.pokemon_number = randint(1,1000)
-        self.img = self.get_img()
-        self.name = self.get_name()
-        self.navik = self.get_ability_name()
-        Pokemon.pokemons[pokemon_trainer] = self
-
-    # Метод для получения картинки покемона через API
-    def get_img(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['sprites']['front_default'])
-        else:
-            return "Pikachu"
-
+@bot.message_handler(commands=['go'])
+def go(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        chance = randint(1,2)
+        if chance == 1:
+            pokemon = Wizard(message.from_user.username)
+            a = 'Wizard'
+        elif chance == 2:
+            pokemon = Fighter(message.from_user.username)
+            a = 'Wizard'
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_message(message.chat.id, f'Класс: {a}')
+        bot.send_photo(message.chat.id, pokemon.show_img())
+        #bot.send_message(message.chat.id, pokemon.show_img())
+        #bot.send_message(message.chat.id, pokemon.skill())
+    else:
+        bot.reply_to(message, "Ты уже создал себе покемона")
+            
+@bot.message_handler(commands=['info'])
+def inform(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
+    else:
+        bot.send_message(message.chat.id, f'У вас нет покемона, сначала получите его')
     
-    # Метод для получения имени покемона через API
-    def get_name(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['forms'][0]['name'])
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
         else:
-            return "Pikachu"
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 
-    def get_type(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['types']['slot'][1]['type']['name'])
-        else:
-            return "Pikachu"
-    def get_ability_name(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['abilities'][0]['ability']['name'])
-    # Метод класса для получения информации
-    def info(self):
-        return f"Имя твоего покеомона: {self.name}"
-    def skill(self):
-        return f"Навыки твоего покеомона: {self.navik}"
-    # Метод класса для получения картинки покемона
-    def show_img(self):
-        return self.img
-    def show_img(self):
-        return f"{self.name}"
+@bot.message_handler(commands=['info'])
+def inform(message):
+    bot.send_message(message.chat.id, )
 
-
+bot.infinity_polling(none_stop=True)
 
